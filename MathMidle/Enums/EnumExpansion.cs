32ea86adaad62ed75da3d84c0dd9a52bcde4cb61
Navarro <altnavarro@gmail.com>
2017-04-13 18:25:03 +0300

    Старт приложения Проверка аргументов

---
+++ MathMidle/Enums/EnumExpansion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMidle.Enums
+{
+    public static class EnumExpansion
+    {
+        public static string GetDescription(this Enum value)
+        {
+            DescriptionAttribute[] da = (DescriptionAttribute[])(value
+                    .GetType()
+                    .GetField(value.ToString())
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false));
+            return da.Length > 0 ? da[0].Description : value.ToString();
+        }
+    }
+}
--- MathMidle/Program.cs
+++ MathMidle/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MathMidle.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,73 @@ namespace MathMidle
 {
     class Program
     {
+        static Commands command = Commands.None;
+        static List<int> numbers = new List<int>();
+
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.Write(ErrorInputMessage());
+            }
+            else
+            {
+                ArgumentsParse(args, numbers);
+            }
+
+            Console.Write("\r\nДля завершения работы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+
+        private static string ErrorInputMessage()
+        {
+            var am = Commands.Algebraic.GetDescription();
+            var gm = Commands.Geometric.GetDescription();
+
+            return string.Format("{0}\r\n{1}\r\n{2}\r\n{3}\r\n{4}\r\n",
+                "Пожалуйста, вызовите программу с одним из следующих аргументов",
+                $"{am} <список чисел> - вычисляет среднее арифметическое для списка чисел",
+                $"Разрешеются только положительные числа. Например: {am} 1 2 3 4 5",
+                $"{gm} <список чисел> - вычисляет среднее арифметическое для списка чисел",
+                $"Разрешеются только положительные числа. Например: {gm} 1 2 3 4 5"
+                );
+        }
+
+        private static void ArgumentsParse(string[] args, List<int> numbers)
+        {
+            if (!(args.Length > 2 && CheckCommand(args[0]) && CheckValues(args, numbers)))
+            {
+                Console.Write($"Вы ввели не правильные аргументы\r\n{ErrorInputMessage()}");
+                ArgumentsParse(Console.ReadLine().Split(' '), numbers);
+            }
+        }
+
+        private static bool CheckValues(string[] args, List<int> numbers)
+        {
+            for (var i = 1; i < args.Length; i++)
+            {
+                var number = 0;
+                if (!int.TryParse(args[i], out number))
+                {
+                    return false;
+                }
+                numbers.Add(number);
+            }
+
+            return true;
+        }
+
+        private static bool CheckCommand(string check)
+        {
+            foreach (Commands cmd in Enum.GetValues(typeof(Commands)))
+            {
+                if (check == cmd.GetDescription())
+                {
+                    command = cmd;
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
