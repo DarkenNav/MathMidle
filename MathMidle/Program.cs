6cf0ac772c74331facffe86a61633532eaf9a339
Navarro <altnavarro@gmail.com>
2017-04-14 11:29:46 +0300

    Добавлена обработка команды Exit - "-q"

--- MathMidle/Program.cs
+++ MathMidle/Program.cs
@@ -1,11 +1,6 @@
 ﻿using MathMidle.Enums;
 using MathMidle.Implementation;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MathMidle
 {
@@ -16,25 +11,21 @@ namespace MathMidle
 
         static void Main(string[] args)
         {
+            #region Test arguments
             //args = new []{ "-am", "1", "2", "3", "4", "5" };
+            //args = new[] { "-am" };
             //args = new[] { "-gm", "1", "2", "3", "3", "3" };
+            #endregion 
 
             if (args.Length == 0)
             {
-                Console.Write(ErrorInputMessage());
+                Console.Write(ErrorInputMessage(false));
             }
             else
             {
-                ArgumentsParse(args);
-                
-                switch (command)
+                if (ArgumentsParse(args))
                 {
-                    case Commands.Algebraic:
-                        Console.WriteLine($"Результат: {numbers.MidleAlgebraic.ToString("N2")}");
-                        break;
-                    case Commands.Geometric:
-                        Console.WriteLine($"Результат: {numbers.MidleGeometric.ToString("N2")}");
-                        break;
+                    Calculate();
                 }
             }
 
@@ -42,46 +33,65 @@ namespace MathMidle
             Console.ReadKey();
         }
 
-        private static string ErrorInputMessage()
+        private static void Calculate()
+        {
+            var result = 0d;
+            switch (command)
+            {
+                case Commands.Algebraic:
+                    result = numbers.MidleAlgebraic;
+                    break;
+                case Commands.Geometric:
+                    result = numbers.MidleGeometric;
+                    break;
+            }
+            Console.WriteLine($"Результат: {result.ToString("N2")}");
+        }
+
+        private static string ErrorInputMessage(bool withExit = true)
         {
             var am = Commands.Algebraic.GetDescription();
             var gm = Commands.Geometric.GetDescription();
 
-            return string.Format("{0}\r\n{1}\r\n{2}\r\n{3}\r\n{4}\r\n",
-                "Пожалуйста, вызовите программу с одним из следующих аргументов",
-                $"{am} <список чисел> - вычисляет среднее арифметическое для списка чисел",
-                $"Разрешеются только положительные числа. Например: {am} 1 2 3 4 5",
-                $"{gm} <список чисел> - вычисляет среднее арифметическое для списка чисел",
-                $"Разрешеются только положительные числа. Например: {gm} 1 2 3 4 5"
+            return string.Format("{0}{1}{2}{3}{4}{5}",
+                "Пожалуйста, вызовите программу с одним из следующих аргументов\r\n",
+                $"{am} <список чисел> - вычисляет среднее арифметическое для списка чисел\r\n",
+                $"Разрешеются только положительные числа. Например: {am} 1 2 3 4 5\r\n",
+                $"{gm} <список чисел> - вычисляет среднее арифметическое для списка чисел\r\n",
+                $"Разрешеются только положительные числа. Например: {gm} 1 2 3 4 5\r\n",
+                (withExit)? $"Для выхода введите {Commands.Exit.GetDescription()}\r\n" : string.Empty
                 );
         }
 
-        private static void ArgumentsParse(string[] args)
+        private static bool ArgumentsParse(string[] args)
         {
-            if (!(args.Length > 2 && CheckCommand(args[0]) && CheckValues(args)))
+            if (args.Length > 0 && CheckCommand(args[0]))
             {
-                Console.Write($"Вы ввели не правильные аргументы\r\n{ErrorInputMessage()}");
-                ArgumentsParse(Console.ReadLine().Split(' '));
+                if (command == Commands.Exit)
+                {
+                    return false;
+                }
+                if ((command == Commands.Algebraic || command == Commands.Geometric)
+                    && args.Length > 2 && CheckValuesFromCalculate(args))
+                {
+                    return true;
+                }
             }
+
+            Console.Write($"Вы ввели не правильные аргументы\r\n{ErrorInputMessage()}");
+            return ArgumentsParse(Console.ReadLine().Split(' '));
         }
 
-        private static bool CheckValues(string[] args)
+        private static bool CheckValuesFromCalculate(string[] args)
         {
             numbers = new NumberList();
             for (var i = 1; i < args.Length; i++)
             {
-                var number = 0;
-                if (!int.TryParse(args[i], out number))
+                if (!numbers.AddWithCheck(args[i]))
                 {
                     return false;
                 }
-                if (number < 1)
-                {
-                    return false;
-                }
-                numbers.Add(number);
             }
-
             return true;
         }
 
